81c0d3ab4d5faa727ea2e37b866b8f86071ef920
DK\ili <ili@sitecore.net>
2016-12-30 10:56:23 +0200

    Patch release

---
+++ src/Sitecore.Support.141041/LanguageResolver.cs
@@ -0,0 +1,74 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using System;
+using System.Web;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Configuration;
+namespace Sitecore.Support.Pipelines.HttpRequest
+{
+    public class LanguageResolver : Sitecore.Pipelines.HttpRequest.LanguageResolver
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            Language result;
+            Assert.ArgumentNotNull(args, "args");
+            Language languageFromRequest = this.GetLanguageFromRequest(args.Context.Request);
+            if (languageFromRequest == null)
+            {
+                if (args.Context.Request.Cookies["sc_lang"] == null || String.IsNullOrEmpty(args.Context.Request.Cookies["sc_lang"].Value))
+                {
+                    Language.TryParse(Settings.DefaultLanguage, out result);
+                    Context.Language = result;
+                    return;
+                }
+                Language.TryParse(args.Context.Request.Cookies["sc_lang"].Value, out result);
+                languageFromRequest = result;
+                if (languageFromRequest == null)
+                {
+                    Language.TryParse(Settings.DefaultLanguage, out result);
+                    Context.Language = result;
+                    return;
+                }
+            }
+            Context.Language = languageFromRequest;
+            Tracer.Info("Language changed to \"" + languageFromRequest.Name + "\" as the query string of the current request contains language (sc_lang).");
+        }
+
+        /// <summary>
+        /// Extracts the language name from the query string.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        private Language ExtractLanguageFromQueryString(System.Web.HttpRequest request)
+        {
+            string text = request.QueryString["sc_lang"];
+            if (string.IsNullOrEmpty(text))
+            {
+                return null;
+            }
+            Language result;
+            if (!Language.TryParse(text, out result))
+            {
+                return null;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the language from request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        private Language GetLanguageFromRequest(System.Web.HttpRequest request)
+        {
+            Language language = this.ExtractLanguageFromQueryString(request);
+            if (language != null)
+            {
+                return language;
+            }
+            return Context.Data.FilePathLanguage;
+        }
+
+
+    }
+}
---
+++ src/Sitecore.Support.141041/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Web.Authentication;
+using System;
+using System.Web;
+using Sitecore.Pipelines.LoggedIn;
+using Sitecore.Security.Accounts;
+
+namespace Sitecore.Support.Pipelines.LoggedIn
+{
+    public class Ticket : LoggedInProcessor
+    {
+        public override void Process(LoggedInArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            string str = TicketManager.CreateTicket(args.Username, args.StartUrl, args.Persist);
+            if (!string.IsNullOrEmpty(str))
+            {
+                HttpContext current = HttpContext.Current;
+                if (current != null)
+                {
+                    HttpCookie cookie2 = new HttpCookie("sitecore_userticket", str);
+                    cookie2.HttpOnly = true;
+                    cookie2.Expires = args.Persist ? DateTime.UtcNow.Add(Settings.Authentication.ClientPersistentLoginDuration) : DateTime.MinValue;
+                    HttpCookie cookie = cookie2;
+                    current.Response.AppendCookie(cookie);
+                    User user = Sitecore.Security.Accounts.User.FromName(args.Username, false);
+                    HttpCookie langCookie = new HttpCookie("sc_lang", user.Profile.ClientLanguage);
+                    langCookie.HttpOnly = true;
+                    langCookie.Expires = args.Persist ? DateTime.UtcNow.Add(Settings.Authentication.ClientPersistentLoginDuration) : DateTime.MinValue;
+                    current.Response.AppendCookie(langCookie);
+                }
+            }
+        }
+    }
+}
